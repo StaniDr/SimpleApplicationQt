b526fd611fc72159624135578af633039547fe92
StaniDr <drobot_stanislav@mail.ru>
2019-10-24 21:37:08 +0300

    task_1_overload_int_complete

---
+++ task_2_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(int argc, char **argv)
+{
+
+
+    unsigned short x = 65535; 
+   printf("was x: %d \n", x);
+    x = x + 1; 
+    printf("x is now: %d \n", x);
+
+    return 0;
+
+
+}
